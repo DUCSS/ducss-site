472420c3cf1d08a1ec3d80991599b86cb189a9bd
Eoin Houlihan <ehoulih@tcd.ie>
2014-08-28 22:20:08 +0100

    Length validations for events and services.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 128 }
   validates :description, presence: true
   validates :slug, presence: true, uniqueness:true
-  validates :location, presence: true
+  validates :location, presence: true, length: { maximum: 128 }
   validates :date, presence: true
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ActiveRecord::Base
   has_attached_file :image, styles: { normal: "350x350" }
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 128 }
   validates :description, presence: true
   validates :image, attachment_presence: true
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
