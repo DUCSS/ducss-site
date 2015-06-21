1d309dfee561626c7738f16ca6d8fbf75d71a545
Eoin Houlihan <ehoulih@tcd.ie>
2015-06-21 22:51:17 +0100

    Refactor upcoming events code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.where('date > ?', DateTime.now).order(date: :desc).limit(4)
+    @events = upcoming_events
     @services = Service.all
     @message = Message.new
   end
@@ -12,9 +12,15 @@ class HomeController < ApplicationController
       flash[:email] = "Email sent"
       redirect_to root_path
     else
-      @events = Event.where('date > ?', DateTime.now).order(date: :desc).limit(4)
+      @events = upcoming_events
       @services = Service.all
       render :action => 'index'
     end
+  end
+
+  private
+
+  def upcoming_events(limit: 4)
+    Event.where('date > ?', DateTime.now).order(date: :desc).limit(limit)
   end
 end
