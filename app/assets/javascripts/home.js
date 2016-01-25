516a5483b6146492abe613f137140ddda4092480
Calvin Nolan <Calvinnolan@gmail.com>
2016-01-25 17:21:59 +0000

    final nitpick (maybe)

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	});
 
 	$(".title").css({
-	  	"background" : "rgba(255, 139, 15, 0.93)"
+	  	"background" : "rgba(255, 139, 15, 0.85)"
 	});
 
 	for(i = 0; i < noOfSquares; i++) {
@@ -19,19 +19,19 @@ $(document).ready(function() {
 	}
 
 	for(i = 0; i < ($(window).height() / 80); i++) {
-	  	$(".background-right-fix").append("<div class='square background-right-fix-square'><div class='left-triangle " + i + "'></div><div class='right-triangle " + i + "'></div></div>");
+	  	$(".background-right-fix").append("<div class='square background-right-fix-square'><div class='left-triangle " + (i  + (i*2) + 1) + "'></div><div class='right-triangle " + (i  + (i*2) + 2)+ "'></div></div>");
 	}
 
     fadeTriangle(Math.random() < 0.5 ? 0 : 1);
     fadeTriangle(Math.random() < 0.5 ? 0 : 1);
 
 	$(".home-landing").click(function() {
-    endFade = true;
+      endFade = true;
       for(i = 0; i < noOfSquares; i++) {
-        $(".left-triangle." + i).fadeTo(1, 0, function() {
+        $(".left-triangle." + i).fadeTo(200, 0, function() {
           $(".left-triangle." + i).remove();
         });
-        $(".right-triangle." + i).fadeTo(1, 0, function() {
+        $(".right-triangle." + i).fadeTo(200, 0, function() {
           $(".right-triangle." + i).remove();
         });
       }
@@ -44,17 +44,19 @@ $(document).ready(function() {
         $(".content-container").css('display', 'block');
         $("body").css('background', 'white');
 
-        $(".title").fadeTo(1000, 0, function() {
-          $(".landing-page").remove();
+        $(".title").fadeTo(600, 0, function() {
+          $(".landing-page").fadeTo(1000, 0, function() {
+            $(".landing-page").remove();
+          });
         });
-      }, 1000);
+      }, 800);
 	});
 } );
 
 var endFade = false;
 var noOfSquares = (($(window).width() / 80) * ($(window).height() / 80)) + 14;
 var total = 0;
-var fadeLimit = noOfSquares * (15/16);
+var fadeLimit = noOfSquares * 2;
 
 function fadeTriangle(triangleSwitch) {
   if(endFade === false) {
@@ -62,25 +64,23 @@ function fadeTriangle(triangleSwitch) {
     var opacity = Math.random();
 
     if(triangleSwitch === 0) {
-      	var timeToFade = 6000 * Math.abs($(".left-triangle." + id).css("opacity") - opacity) + (total * 20);
+      	var timeToFade = 4000 * Math.abs($(".left-triangle." + id).css("opacity") - opacity) + (total * 20);
       	$(".left-triangle." + id).fadeTo(timeToFade, opacity, function() {
-        	total+=4;
+        	total+=3;
         	if(total < fadeLimit) {
-        		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
+        		  fadeTriangle(Math.random() < 0.5 ? 0 : 1);
           		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
-          		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
-          		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
+              fadeTriangle(Math.random() < 0.5 ? 0 : 1);
         	} 
       	});
     } else {
-      	var timeToFade = 6000 * Math.abs($(".right-triangle." + id).css("opacity") - opacity) + (total * 20);
+      	var timeToFade = 4000 * Math.abs($(".right-triangle." + id).css("opacity") - opacity) + (total * 20);
       	$(".right-triangle." + id).fadeTo(timeToFade, opacity, function() {
-        	total+=4;
+        	total+=3;
         	if(total < fadeLimit) {
           		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
           		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
-          		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
-          		fadeTriangle(Math.random() < 0.5 ? 0 : 1);
+              fadeTriangle(Math.random() < 0.5 ? 0 : 1);
         	}
       	});
     }
