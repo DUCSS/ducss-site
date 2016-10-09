ed7ed5f15fc704a93ebdb3afbf883fc841f1bb28
Patrick O'Boyle <oboylepa@tcd.ie>
2016-10-09 14:40:51 +0100

    Test fun

--- spec/factories/internships.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :internships do
-    job_title "Software Engineering Intern"
-    company_name "DUCSS"
-    job_location "Dublin, Ireland"
-    apply_link "http://ducss.ie"
-    company_logo { fixture_file_upload "#{Rails.root}/spec/fixtures/images/profile-pic-test.jpg", 'image/jpg' }
-  end
-end
