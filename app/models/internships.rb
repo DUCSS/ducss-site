2e0ae516454fff7c6541b0030143ded99cbd526a
Patrick O'Boyle <oboylepa@tcd.ie>
2016-11-19 17:17:48 +0000

    Increased max length of job titles & application links for internships

--- app/models/internships.rb
+++ app/models/internships.rb
@@ -5,11 +5,11 @@ class Internships < ActiveRecord::Base
 
   URL_VALIDATION_REGEX = /\A(?:(?:https?|ftp):\/\/)(?:\S+(?::\S)?@)?(?:(?!10(?:.\d{1,3}){3})(?!127(?:.\d{1,3}){3})(?!169.254(?:.\d{1,3}){2})(?!192.168(?:.\d{1,3}){2})(?!172.(?:1[6-9]|2\d|3[0-1])(?:.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)[a-z\u00a1-\uffff0-9]+)(?:.(?:[a-z\u00a1-\uffff0-9]+-?)[a-z\u00a1-\uffff0-9]+)(?:.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?\z/i
 
-  validates :job_title, presence: true, length: { maximum: 128 }
+  validates :job_title, presence: true, length: { maximum: 256 }
   validates :company_name, presence: true
   validates :job_location, presence: true
   validates :company_logo, attachment_presence: true
-  validates :apply_link, presence: true, length: { maximum: 128 }, format: { with: URL_VALIDATION_REGEX }
+  validates :apply_link, presence: true, length: { maximum: 512 }, format: { with: URL_VALIDATION_REGEX }
   validates_attachment_content_type :company_logo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   def self.open_internships
