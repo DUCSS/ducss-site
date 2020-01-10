d7c8d2fd8ef64677e762f5502177b7ee02ff39e1
Ernest Kuznecovs <ernest@Ernests-MacBook-Pro.local>
2020-01-10 04:01:00 +0000

    installed mongodb

--- app.js
+++ app.js
@@ -1,59 +1,41 @@
 const express = require('express');
 const app = express();
-
 app.use(express.static("public"));
-
 app.set('view engine', 'ejs');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://basket:3448fc047e4657d16f6acb14b247b860@dokku-mongo-basket:27017/basket';
+// Create a new MongoClient
+const client = new MongoClient(url);
+// Database Name
+const dbName = 'baseket';
+const internshipPosts;
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  db.collection('internships').find().toArray(function (err, result) {
+    if (err) throw err
+    internshipPosts = result;
+  })
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-// app.get('/events', (req, res) => {
-//   res.render('events');
-// });
-
-// app.get('/esports', (req, res) => {
-//   res.render('esports');
-// });
-
-// app.get('/minecraft', (req, res) => {
-//   res.render('minecraft');
-// });
-
 app.get('/contact', (req, res) => {
   res.render('contact');
 });
-
-// will be aranged so that id 0 is the very last internship on the webpage
-var internshipPosts = [
-  {thumbnail:  "https://tentulogo.com/wp-content/uploads/microsoft-logo.jpg"
-  ,title: "Microsoft Early Careers"
-  ,description: "Full-time software engineering and intern software engineering positions available at Microsoft. Information can be found here: https://www.facebook.com/ducss.ie/posts/2587060181571230 and https://drive.google.com/file/d/1bkwat6tp3osycgwMby7BImIKnEVVR31Z/view?usp=sharing"
-  ,datePosted: "15th October 2019"
-  ,id: 2	
-  },
-  {thumbnail:  "https://www.computing.dcu.ie/sites/default/files/images/Arista%20Logo-7.preview.jpg"
-  ,title: "Recruiting Graduate Software Engineers for 2020"
-  ,description: "Arista Networks is looking for great Graduate/Intern Engineers interested in product development in C/C++ and Go to join the team in Dublin advancing the Software Defined Networking (SDN) revolution. For more info: https://drive.google.com/file/d/17c_Vgsh2fDGFhvogYDtWhZH-fdIvbvU8/view?usp=sharing"
-  ,datePosted: "23rd September 2019"
-  ,id: 1
-  },
-  {thumbnail:  "https://ci6.googleusercontent.com/proxy/H7Y10GSxbJ5arVsjOXXl31jT684OGYqCivrafo1Fz5D0AZujUImCWZgvb-Be-nTSrxyIVeeLbkWtVqB3UpakhzRU0FfHuy6MmMpTkcIuLsNkjOIIlPWPO-lOJ7FOA2FTE3Fq9xH8vqr0PTpOaYhdlig=s0-d-e1-ft#http://d31hzlhk6di2h5.cloudfront.net/20190908/0b/fe/e4/be/1aa382e688eb3ee2638e4464_560x294.jpg"
-    ,title: "SIG Job Opportunities"
-    ,description: "Our proud sponsor Susquehanna International Group has many internship opportunities available. More on that, and their graduate programmes (both SW and Infrastructure) can be found in this folder: https://drive.google.com/drive/folders/181xL7ksxSz06nPi1eHKCvefGTOYUqHN2?usp=sharing"
-    ,datePosted: "8th September 2019"
-    ,id: 0
-  }
-];
-
 
 app.get('/internships', (req, res) => {
   res.render('internships', {internshipPosts: internshipPosts});
 });
 
 // process.env.PORT is the port of the server(DigitalOcean) that is listening
-
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '0.0.0.0';
 app.listen(PORT, HOST, () =>{
