0ea5b7b1972265bcc7e895ed7e2b812de4f3eb26
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-09-28 23:06:09 +0100

    Add internship model

---
+++ server/src/models/InternshipEntry.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const requiredString = {
+  type: String,
+  required: true,
+}
+
+const internshipEntrySchema = new Schema({
+  title: requiredString,
+  shortDescription: requiredString,
+  links: [{ desciption: String, link: requiredString }],
+});
+
+export const InternshipEntry = mongoose.model(
+  "InternshipEntry",
+  internshipEntrySchema
+);
