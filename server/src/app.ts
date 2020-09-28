d32f4a6d2169a49f967e0bc35eaa813c77724f27
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-09-28 19:10:38 +0100

    Add error handler middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,15 @@
-import express, { Request, Response } from "express";
-const app = express();
+import express, { Request, Response } from 'express';
+import {notFoundError, errorHandler } from './middlewares'
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'https://localhost:3000',
+}))
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200);
@@ -11,6 +17,9 @@ app.get("/", (req: Request, res: Response) => {
     response: "Hello world",
   });
 });
+
+app.use(notFoundError);
+app.use(errorHandler);
 
 const PORT = process.env.API_PORT;
 const HOST = process.env.HOST || "0.0.0.0";
