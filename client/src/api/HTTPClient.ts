b8fbf46d1d32c1a504ad74a6ef720ee860177340
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-09-29 23:32:21 +0100

    Add skeleton api code

---
+++ client/src/api/HTTPClient.ts
@@ -0,0 +1,9 @@
+const BASE_URL = `http://localhost:${process.env.SERVER_PORT}`;
+
+class HTTPClient {
+  async request(method: string, route: string) {}
+
+  async getInternshipEntries() {}
+}
+
+export default HTTPClient;
