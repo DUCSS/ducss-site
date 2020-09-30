23c8db8b59f3e320745da50f9f18d5132a3d1cd6
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-09-30 23:15:19 +0100

    Add getInternshipEntries

--- client/src/api/HTTPClient.ts
+++ client/src/api/HTTPClient.ts
@@ -1,9 +1,24 @@
-const BASE_URL = `http://localhost:${process.env.SERVER_PORT}`;
+const BASE_URL = `http://localhost:${process.env.SERVER_PORT}/api/v1`;
 
 class HTTPClient {
-  async request(method: string, route: string) {}
+  request(method: string, route: string) {
+    const url = BASE_URL + route;
 
-  async getInternshipEntries() {}
+    return fetch(url, {
+      method,
+    });
+  }
+
+  async getInternshipEntries() {
+    const response = await this.request("GET", "/internships");
+    const json = await response.json();
+
+    if (response.ok) {
+      return json.response;
+    }
+
+    throw new Error(json.error);
+  }
 }
 
 export default HTTPClient;
