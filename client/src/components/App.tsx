39397b99eaa76ae7127ad0e7fa3d8588d0412104
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-10-01 00:04:36 +0100

    Add initial code

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import HTTPClient from "../api/http_client";
+import { InternshipEntry } from "../interfaces/database_types";
 
 const App = () => {
+  const [internshipEntries, setInternshipEntries] = useState<[InternshipEntry] | undefined>();
+  const [httpClient] = useState<HTTPClient>(new HTTPClient());
+  useEffect(() => {
+    (async () => {
+      const internshipEntries = await httpClient.getInternshipEntries();
+      setInternshipEntries(internshipEntries);
+    })()
+  }, [httpClient]);
+
   return (
     <div>
-      <h1>Hello world</h1>
+      <ul>
+        {internshipEntries?.map((entry: InternshipEntry) => {
+          return <li>{entry.title}</li>
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
