a6394dda13403bc018bbbe006b342326bd63d72e
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-10-01 18:08:50 +0100

    Format code

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,20 +3,22 @@ import HTTPClient from "../api/http_client";
 import { InternshipEntry } from "../interfaces/database_types";
 
 const App = () => {
-  const [internshipEntries, setInternshipEntries] = useState<[InternshipEntry] | undefined>();
+  const [internshipEntries, setInternshipEntries] = useState<
+    [InternshipEntry] | undefined
+  >();
   const [httpClient] = useState<HTTPClient>(new HTTPClient());
   useEffect(() => {
     (async () => {
       const internshipEntries = await httpClient.getInternshipEntries();
       setInternshipEntries(internshipEntries);
-    })()
+    })();
   }, [httpClient]);
 
   return (
     <div>
       <ul>
         {internshipEntries?.map((entry: InternshipEntry) => {
-          return <li>{entry.title}</li>
+          return <li>{entry.title}</li>;
         })}
       </ul>
     </div>
