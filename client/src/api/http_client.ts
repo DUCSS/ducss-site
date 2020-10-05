785a74148f75b4dbbac0f5d9ac9da33ede6bdfd2
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-10-05 23:56:12 +0100

    Add airbnb style

--- client/src/api/http_client.ts
+++ client/src/api/http_client.ts
@@ -3,7 +3,7 @@ import { InternshipEntry } from '../interfaces/database_types';
 const BASE_URL = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_SERVER_PORT}/api/v1`;
 
 class HTTPClient {
-  request(method: string, route: string): Promise<Response> {
+  static request(method: string, route: string): Promise<Response> {
     const url = BASE_URL + route;
 
     return fetch(url, {
@@ -11,8 +11,8 @@ class HTTPClient {
     });
   }
 
-  async getInternshipEntries(): Promise<[InternshipEntry]> {
-    const response = await this.request('GET', '/internships');
+  static async getInternshipEntries(): Promise<[InternshipEntry]> {
+    const response = await HTTPClient.request('GET', '/internships');
     const json = await response.json();
 
     if (response.ok) {
