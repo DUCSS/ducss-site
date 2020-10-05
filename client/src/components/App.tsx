0428b5d3e9cfd213f3bd762a08b364ae3d0e3848
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-10-05 23:56:12 +0100

    Add airbnb style

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -9,42 +9,34 @@ const App = () => {
   const [httpClient] = useState<HTTPClient>(new HTTPClient());
   useEffect(() => {
     (async () => {
-      const internshipEntries = await httpClient.getInternshipEntries();
-      setInternshipEntries(internshipEntries);
+      const entries = await HTTPClient.getInternshipEntries();
+      setInternshipEntries(entries);
     })();
   }, [httpClient]);
 
   return (
     <div>
       <ul>
-        {internshipEntries?.map(
-          (entry: InternshipEntry, internshipIndex: number) => {
-            return (
-              <li key={`internship-entry-${internshipIndex}`}>
-                <p>{entry.title}</p>
-                <p>{entry.shortDescription}</p>
-                <ul>
-                  {entry.links.map(
-                    (linkEntry: LinkEntry, linkIndex: number) => {
-                      return (
-                        <li key={`link-entry-${internshipIndex}-${linkIndex}`}>
-                          {linkEntry.description} -{' '}
-                          <a
-                            href={linkEntry.link}
-                            rel="noopener noreferrer"
-                            target="_blank"
-                          >
-                            Apply Here
-                          </a>
-                        </li>
-                      );
-                    }
-                  )}
-                </ul>
-              </li>
-            );
-          }
-        )}
+        {internshipEntries?.map((entry: InternshipEntry) => (
+          <li key={entry.id}>
+            <p>{entry.title}</p>
+            <p>{entry.shortDescription}</p>
+            <ul>
+              {entry.links.map((linkEntry: LinkEntry) => (
+                <li key={linkEntry.id}>
+                  {linkEntry.description} -{' '}
+                  <a
+                    href={linkEntry.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Apply Here
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- client/src/interfaces/database_types.ts
+++ client/src/interfaces/database_types.ts
@@ -1,9 +1,11 @@
 export interface LinkEntry {
+  id: number;
   description?: string;
   link: string;
 }
 
 export interface InternshipEntry {
+  id: number;
   title: string;
   shortDescription: string;
   links: [LinkEntry];
