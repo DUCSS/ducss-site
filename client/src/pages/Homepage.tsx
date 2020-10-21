10d0801173bea56c414365dc216e3cec9fa77ad3
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-10-21 11:38:56 +0100

    Add temp homepage

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
 const Homepage: React.FC = () => {
-  return <h1 className="text-center">Homepage</h1>;
+  return (
+    <div className="text-center text-ducss-dark">
+      <Jumbotron className="my-4 bg-ducss-primary">
+        <h1>Dublin University Computer Science Society</h1>
+        <hr className="my-4 bg-ducss-dark" />
+        <p className="lead">A community in Trinity College Dublin.</p>
+      </Jumbotron>
+      <h1>
+        <span role="img" aria-label="Construction Sign">
+          🚧
+        </span>{' '}
+        Under Construction{' '}
+        <span role="img" aria-label="Construction Sign">
+          🚧
+        </span>
+      </h1>
+    </div>
+  );
 };
 
 export default Homepage;
