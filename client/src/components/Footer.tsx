b4811ead6da5d5c2b074e1ab0f59fad618526574
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-10-28 12:41:50 +0000

    Remove twitter link

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,6 @@ import {
   faFacebook,
   faInstagram,
   faDiscord,
-  faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
 
@@ -45,12 +44,6 @@ const Footer: React.FC = () => (
           <a href="https://discord.gg/NUZGYT4" rel="noreferrer" target="_blank">
             <FontAwesomeIcon icon={faDiscord} />
             <span className="sr-only">Discord</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/ducss" rel="noreferrer" target="_blank">
-            <FontAwesomeIcon icon={faTwitter} />
-            <span className="sr-only">Twitter</span>
           </a>
         </li>
       </ul>
